9caed85261da8ba8eb5cc3fe5e81146e6a4ae435
rafimk <rafi.genius.cs@gmail.com>
2021-04-05 13:50:14 +0400

    First commit

---
+++ Bank.Consume/Program.cs
@@ -0,0 +1,16 @@
+﻿using Bank.Lib;
+using System;
+
+namespace Bank.Consume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount(1000);
+            account.Add(100);
+
+            Console.WriteLine($"Your balance is {account.Balance.ToString()} ");
+        }
+    }
+}
